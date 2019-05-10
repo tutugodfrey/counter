df0ac3b9e6eb7a26e662c2c572772ff067fff246
Godfrey Tutu <godfreytutu@Godfreys-MacBook-Pro.local>
2019-05-11 00:25:07 +0100

    modify main.js to render multiple windows

--- src/main.js
+++ src/main.js
@@ -5,24 +5,27 @@ const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 const ipc = electron.ipcMain;
 
-let mainWindow
+const windows = [];
 app.on('ready', _ => {
-  mainWindow = new BrowserWindow({
-    height: 400,
-    width: 400
-  });
-
-  mainWindow.loadURL(`file://${__dirname}/countdown.html`)
-
-  mainWindow.on('closed', _ => {
-    console.log('Cloded!');
-    mainWindow = null
+  [1, 2, 3].forEach(_ => {
+    let win = new BrowserWindow({
+      height: 400,
+      width: 400
+    });
+  
+    win.loadURL(`file://${__dirname}/countdown.html`)
+  
+    win.on('closed', _ => {
+      mainWindow = null
+    })
+    windows.push(win)
   })
 })
 
 ipc.on('countdown-start', _ => {
-  console.log('worked');
   countdown(count => {
-    mainWindow.webContents.send('countdown', count)
+    windows.forEach(win => {
+      win.webContents.send('countdown', count)
+    })
   })
 })
--- src/renderer.js
+++ src/renderer.js
@@ -1,7 +1,7 @@
 const electron = require('electron');
 const ipc = electron.ipcRenderer;
+
 document.getElementById('start').addEventListener('click', _ => {
-  console.log('start clicked');
   ipc.send('countdown-start')
 });
 
